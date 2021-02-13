65af3f6c8a7c0b6ef218c13bab52ff4300e972a0
tacticaltofu <dingkevin@gmail.com>
2021-02-13 15:39:01 -0500

    Image upload is now working

--- src/models/image.js
+++ src/models/image.js
@@ -6,9 +6,20 @@ const imageSchema = new mongoose.Schema({
         required: true,
         trim: true
     },
+    file: {
+        type: Buffer,
+        required: true
+    }
 }, {
     timestamps: true
 })
+
+imageSchema.methods.toJSON = function () {
+    const image = this
+    const imageObj = image.toObject()
+    delete imageObj.file
+    return imageObj
+}
 
 const Image = new mongoose.model('Image', imageSchema)
 
--- src/routers/image.js
+++ src/routers/image.js
@@ -1,15 +1,81 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const Image = require('../models/image')
 
 const router = express.Router()
 
-router.post('/', async (req, res) => {
+const upload = multer({
+    fileSize: 1000000
+})
+
+router.post('/', upload.single('image'), async (req, res) => {
     try {
-        const image = new Image(req.body)
+        const buffer = await sharp(req.file.buffer).png().toBuffer()
+        const image = new Image({ 
+            title: req.body.title, 
+            file: buffer 
+        })
         await image.save()
-        res.status(201).send(image)
+        res.status(201).send()
     } catch (e) {
-        res.status(400).send()
+        res.status(400).send(e)
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const image = await Image.findById(req.params.id)
+
+        if (!image) {
+            return res.status(404).send()
+        }
+
+        res.send(image)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/:id/file', async (req, res) => {
+    try {
+        const image = await Image.findById(req.params.id)
+
+        if (!image) {
+            return res.status(404).send()
+        }
+
+        res.set('Content-Type', 'image/png').send(image.file)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const image = await Image.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        
+        if (!image) {
+            return res.status(404).send()
+        }
+        await image.save()
+        res.send(image)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const image = await Image.findByIdAndDelete(req.params.id)
+        
+        if (!image) {
+            return res.status(404).send()
+        }
+
+        res.send(image)
+    } catch (e) {
+        res.status(400).send(e)
     }
 })
 
